5b308dc48e9b7c9bf1a62f4e0755c9721ec6a0e1
kohinurfatema <kohinurfatema35@gmail.com>
2026-03-17 06:45:33 -0700

    feat: add Header with active nav links and Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,15 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
-import { useEffect, useState } from 'react'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import Home from './pages/Home/Home'
-import AllApps from './pages/AllApps/AllApps'
-import AppDetails from './pages/AppDetails/AppDetails'
-import MyInstallation from './pages/MyInstallation/MyInstallation'
-import ErrorPage from './pages/ErrorPage/ErrorPage'
-import Loading from './components/Loading/Loading'
 import './App.css'
 
 function App() {
-  const location = useLocation()
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    const timer = setTimeout(() => setLoading(false), 600)
-    return () => clearTimeout(timer)
-  }, [location.pathname])
-
   return (
     <div className="app-wrapper">
       <Header />
       <main className="main-content">
-        {loading ? (
-          <Loading />
-        ) : (
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/apps" element={<AllApps />} />
-            <Route path="/apps/:id" element={<AppDetails />} />
-            <Route path="/installation" element={<MyInstallation />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        )}
+        <div style={{ textAlign: 'center', padding: '80px 24px', color: '#64748b' }}>
+          <h2>Pages coming soon...</h2>
+        </div>
       </main>
       <Footer />
     </div>
